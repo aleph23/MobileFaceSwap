f9dc599076b8c90f184f2fb09ded4d91e589eb91
Admin <>
2023-04-29 20:40:35 +0900

    [add]

--- DLpj_models.py
+++ DLpj_models.py
@@ -1,5 +1,5 @@
 from facenet_pytorch import InceptionResnetV1
-from face_detector import YoloDetector
+from yoloface.face_detector import YoloDetector
 import cv2
 import torch
 from utils.align_face import align_img
